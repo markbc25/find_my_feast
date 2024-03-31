c90d0d2f9c5296906741d75fa6f3fb4f2c95a964
alexiahassan <alexiahassan@tamu.edu>
2024-03-31 14:50:56 -0500

    no persistent sign in

--- client/App.tsx
+++ client/App.tsx
@@ -74,51 +74,49 @@ function App(): React.JSX.Element {
   useEffect(() => {
     LottieSplashScreen.hide();
 
-    storage
-      .load({
-        key: 'loginState',
+    // storage
+    //   .load({
+    //     key: 'loginState',
 
-        // autoSync (default: true) means if data is not found or has expired,
-        // then invoke the corresponding sync method
-        autoSync: true,
+    //     // autoSync (default: true) means if data is not found or has expired,
+    //     // then invoke the corresponding sync method
+    //     autoSync: true,
 
-        // syncInBackground (default: true) means if data expired,
-        // return the outdated data first while invoking the sync method.
-        // If syncInBackground is set to false, and there is expired data,
-        // it will wait for the new data and return only after the sync completed.
-        // (This, of course, is slower)
-        syncInBackground: true,
+    //     // syncInBackground (default: true) means if data expired,
+    //     // return the outdated data first while invoking the sync method.
+    //     // If syncInBackground is set to false, and there is expired data,
+    //     // it will wait for the new data and return only after the sync completed.
+    //     // (This, of course, is slower)
+    //     syncInBackground: true,
 
-        // you can pass extra params to the sync method
-        // see sync example below
-        syncParams: {
-          extraFetchOptions: {
-            // blahblah
-          },
-          someFlag: true
-        }
-      })
-      .then(ret => {
-        // found data go to then()
-        console.log(ret.userid);
-        setIsSignedIn(true);
-      })
-      .catch(err => {
-        // any exception including data not found
-        // goes to catch()
-        console.warn(err.message);
-        switch (err.name) {
-          case 'NotFoundError':
-            setIsSignedIn(false);
-            break;
-          case 'ExpiredError':
-            setIsSignedIn(false);
-            break;
-        }
-      });
+    //     // you can pass extra params to the sync method
+    //     // see sync example below
+    //     syncParams: {
+    //       extraFetchOptions: {
+    //         // blahblah
+    //       },
+    //       someFlag: true
+    //     }
+    //   })
+    //   .then(ret => {
+    //     // found data go to then()
+    //     console.log(ret.userid);
+    //     setIsSignedIn(true);
+    //   })
+    //   .catch(err => {
+    //     // any exception including data not found
+    //     // goes to catch()
+    //     console.warn(err.message);
+    //     switch (err.name) {
+    //       case 'NotFoundError':
+    //         setIsSignedIn(false);
+    //         break;
+    //       case 'ExpiredError':
+    //         setIsSignedIn(false);
+    //         break;
+    //     }
+    //   });
   }, []);
-
-
 
 
   return (
