c082828be6e6b0af53f2c9b46c69c673675439a4
alexiahassan <alexiahassan@tamu.edu>
2024-04-13 19:21:00 -0500

    logout working + error messages shown on sign in sign up

--- client/src/screens/ProfileView/ProfileView.tsx
+++ client/src/screens/ProfileView/ProfileView.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View, Button} from 'react-native';
+import { Text, View, Button } from 'react-native';
 import InputText from '../../../src/components/InputText/InputText';
 import LineBreakIcon from '../../../src/components/LineBreakIcon/LineBreakIcon';
 import ToggleButton from '../../../src/components/ToggleButton/ToggleButton';
@@ -8,67 +8,67 @@ import ScreenTitle from '../../../src/components/ScreenTitle/ScreenTitle';
 import SectionTitle from '../../../src/components/SectionTitle/SectionTitle';
 import ActionButton from '../../../src/components/ActionButton/ActionButton';
 
-const ProfileScreen = () => {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: '',
-//         alignItems: 'center',
-      }}>
 
-        <View style = {{flex: 0.3, paddingVertical: 30, paddingHorizontal: 30}}>
-            <ScreenTitle textValue = 'Profile'></ScreenTitle>
-        </View>
+interface ProfileViewProps {
+    onLogoutButtonPressed: Function,
+}
 
-
-        <View style = {{flex: 0.5, justifyContent: 'center', paddingHorizontal: 30}}>
-            <SectionTitle textValue = 'Change Name'></SectionTitle>
-
-            <View style = {{
-                justifyContent: 'flexStart',
-                alignSelf: 'flexStart',
-                paddingVertical: 30,
-                }}>
-                <InputText fieldName = 'DISPLAY NAME' textValue = "John Smith"></InputText>
-            </View>
-        </View>
-
-
-    <View style = {{flex: 0.35,}}>
-        <View style = {{
-                padding: 20,
-                alignSelf: 'stretch',
-                justifyContent: 'center',
+const ProfileScreen = (props: ProfileViewProps) => {
+    return (
+        <View
+            style={{
                 flex: 1,
+
+                //         alignItems: 'center',
             }}>
-            <LineBreakIcon></LineBreakIcon>
+
+            <View style={{ paddingVertical: 30, paddingHorizontal: 30 }}>
+                <ScreenTitle textValue='Profile'></ScreenTitle>
+            </View>
+
+
+
+            <View style={{ paddingHorizontal: 30 }}>
+                <SectionTitle textValue="Dietary Preferences"></SectionTitle>
+
+                <View style={{
+                    justifyContent: 'flex-start',
+                    alignSelf: 'stretch',
+                    padding: 15,
+                    paddingBottom: 60,
+
+                }}>
+                    <ToggleableSetting textValue="Vegetarian"></ToggleableSetting>
+
+                    <ToggleableSetting textValue="Vegan"></ToggleableSetting>
+                </View>
+            </View>
+
+            <View style={{ flex: 0, width: '90%', alignSelf: 'center', justifyContent: 'center' }}>
+                {/* todo: send changes to database on press */}
+                <ActionButton textValue='Confirm'></ActionButton>
+            </View>
+
+            <View style={{}}>
+                <View style={{
+                    padding: 20,
+                    alignSelf: 'stretch',
+                    justifyContent: 'center',
+                }}>
+                    <LineBreakIcon></LineBreakIcon>
+                </View>
+            </View>
+
+            <View style={{ flex: 0.9, justifyContent: 'flex-end' }}>
+
+
+                <View style={{ flex: 0, width: '90%', alignSelf: 'center', justifyContent: 'center' }}>
+                    <ActionButton onPress={props.onLogoutButtonPressed} textValue='Logout'></ActionButton>
+                </View>
+            </View>
+
         </View>
-    </View>
-
-
-    <View style = {{flex: 1.5, paddingHorizontal: 30}}>
-        <SectionTitle textValue = "Dietary Preferences"></SectionTitle>
-
-        <View style = {{
-            justifyContent: 'flex-start',
-            alignSelf: 'stretch',
-//             padding: 15,
-            paddingBottom: 60,
-            flex: 1,
-            }}>
-                 <ToggleableSetting textValue = "Vegetarian"></ToggleableSetting>
-                 <ToggleableSetting textValue = "Pescatarian"></ToggleableSetting>
-                 <ToggleableSetting textValue = "Vegan"></ToggleableSetting>
-                 <ToggleableSetting textValue = "Gluten Free"></ToggleableSetting>
-        </View>
-    </View>
-
-    <View style = {{flex: 0.5, width: '90%', alignSelf: 'center', justifyContent: 'center'}}>
-            <ActionButton textValue = 'Confirm'></ActionButton>
-    </View>
-  </View>
-  );
+    );
 };
 
 export default ProfileScreen;
